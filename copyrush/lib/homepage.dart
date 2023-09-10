1de586e3de9f9c954b107220de276a433e5c7707
Justsah1 <sahilbomble.097@gmail.com>
2023-09-10 17:11:31 +0530

    ui changed

--- copyrush/lib/homepage.dart
+++ copyrush/lib/homepage.dart
@@ -7,22 +7,40 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text(
-          "CopyRush",
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        leading: Icon(Icons.menu),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text("Copyrush"),
-            Text("Copyrush"),
-          ],
-        ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          Container(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 50),
+            child: Row(children: [
+              Icon(
+                Icons.location_pin,
+                size: 50,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Data",
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    "data",
+                    style: TextStyle(fontWeight: FontWeight.w400),
+                  ),
+                ],
+              ),
+              Icon(
+                Icons.arrow_drop_down,
+                size: 35,
+              ),
+              Container(
+                  decoration:
+                      BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+                  child: Icon(Icons.person))
+            ]),
+          )
+        ],
       ),
     );
   }
---
+++ copyrush/lib/shopui.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Shopui extends StatefulWidget {
+  const Shopui({super.key});
+
+  @override
+  State<Shopui> createState() => _ShopuiState();
+}
+
+class _ShopuiState extends State<Shopui> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
